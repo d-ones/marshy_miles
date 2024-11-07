505a06fb30e3ebb5db781c0aab1343b8318849c3
duncanrjones <duncan@chadpad>
2024-11-06 22:09:24 -0500

    update for var json formatting

--- main.py
+++ main.py
@@ -1,8 +1,16 @@
 import parse_map_methods
 import vars
 
+payload = {
+    "client_id": vars.client_id,
+    "client_secret": vars.client_secret,
+    "refresh_token": vars.refresh_token,
+    "grant_type": "refresh_token",
+    "f": "json",
+}
+
 dataset = parse_map_methods.return_activity_dataset(
-    vars.auth_url, vars.activities_url, vars.payload
+    vars.auth_url, vars.activities_url, payload
 )
 
 polyline = dataset[0]["map"]["summary_polyline"]
--- vars.py
+++ vars.py
@@ -2,4 +2,6 @@ import os
 
 auth_url = os.environ["auth_url"]
 activities_url = os.environ["activities_url"]
-payload = os.environ["paylod"]
+client_id = os.environ["client_id"]
+client_secret = os.environ["client_secret"]
+refresh_token = os.environ["refresh_token"]
